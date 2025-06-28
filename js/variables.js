be6ab63a8b23dfd04283bb4ae36feb6d7c4b9bf8
ryanstokes21 <Ryanstokes141@gmail.com>
2025-06-27 20:24:40 -0500

    addad variable example to variables.js and

---
+++ js/variables.js
@@ -0,0 +1,52 @@
+/*
+// Example of declaring a variable
+let name = 'John';
+let surname = 'Doe';
+
+console.log(name);
+console.log(surname);
+
+// Example of re-assigning variables
+let age = 11;
+console.log(age);
+
+age = 54;
+console.log(age);
+
+// Example of a constant variable that can't re-assign
+
+const pi = 3.14;
+pi = 10;
+
+console.log(pi);
+
+// Math Example in JS
+console.log((3 + 2) - 76 * (1 + 1));
+*/
+
+
+// Math
+console.log(23 + 97);
+console.log(67 + 97);
+console.log(45 + 97);
+console.log(95 + 97);
+console.log(28 + 97);
+console.log(62 + 97);
+console.log(55 + 97);
+
+console.log((4 + 6 + 9) / 77);
+
+//variables
+let a = 10;
+console.log(a);
+
+a = 4;
+console.log(a);
+
+let b = 7 * a
+console.log(b);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+console.log(percentage);
